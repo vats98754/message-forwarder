751d4df7476f5df32d4934ee6e7c672459c0d69e
vats98754 <a7vats@uwaterloo.ca>
2025-07-16 02:45:09 +0530

    Add Flutter platform support for iOS, web, Windows, Linux, and macOS

---
+++ android/app/src/main/kotlin/com/messageforwarder/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.messageforwarder.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/messageforwarder/message_forwarder_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.messageforwarder.message_forwarder_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
